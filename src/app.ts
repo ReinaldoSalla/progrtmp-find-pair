f78056c8a1fd67ba571514cae77f4044cf2707ae
Reinaldo Salla <reinaldosalla95@gmail.com>
2021-01-15 14:10:08 -0300

    feat: starting to push content for the article

--- src/app.ts
+++ src/app.ts
@@ -11,107 +11,125 @@
  * https://stackoverflow.com/questions/55057200/is-the-set-has-method-o1-and-array-indexof-on
  */
 
-function findPair(
-  arr: Array<number>, 
-  sum: number
-): null | boolean | Array<number> {
-  // Solution with two loop (quadratic)
-  // let pair: Array<number> = [];
-  // for (let i = 0; i < arr.length - 1; i++) {
-  //   for (let j = i + 1; j < arr.length; j++) {
-  //     if (arr[i] + arr[j] === sum) {
-  //       pair = [arr[i], arr[j]];
-  //     }
-  //   }
-  // } 
-  // return pair;
+// function findPair(
+//   arr: Array<number>, 
+//   sum: number
+// ): null | Array<number> {
+//   // Solution with two loop (quadratic)
+//   // let pair: Array<number> = [];
+//   // for (let i = 0; i < arr.length - 1; i++) {
+//   //   for (let j = i + 1; j < arr.length; j++) {
+//   //     if (arr[i] + arr[j] === sum) {
+//   //       pair = [arr[i], arr[j]];
+//   //     }
+//   //   }
+//   // } 
+//   // return pair;
 
-  // Binary Search (logN)
+//   // Binary Search (logN)
 
-  // Shift (Linear)
-  // let low = 0;
-  // const high = arr.length - 1;
-  // while (low < high) {
-  //   console.log(`iteration ${low+1}: arr[low]=${arr[low]} arr[high]=${arr[high]}`)
-  //   if (arr[low] + arr[high] === sum) {
-  //     return [arr[low], arr[high]];
-  //   }
-  //   low++; 
-  // }
+//   // Shift (Linear)
+//   // let low = 0;
+//   // const high = arr.length - 1;
+//   // while (low < high) {
+//   //   console.log(`iteration ${low+1}: arr[low]=${arr[low]} arr[high]=${arr[high]}`)
+//   //   if (arr[low] + arr[high] === sum) {
+//   //     return [arr[low], arr[high]];
+//   //   }
+//   //   low++; 
+//   // }
 
-  // complement (liiner)
-  // const comp = [sum-arr[0]];
-  // for (const item of arr.slice(1)) {
-  //   console.log(`arrItem = ${item}, compArr = ${comp}`);
-  //   const found = comp.find((compItem) => compItem === item);
-  //   if (found) {
-  //     return [item, found];
-  //   }
-  //   comp.push(sum-item);
-  // }
+//   // complement (liiner)
+//   // const comp = [sum-arr[0]];
+//   // for (const item of arr.slice(1)) {
+//   //   console.log(`arrItem = ${item}, compArr = ${comp}`);
+//   //   const found = comp.find((compItem) => compItem === item);
+//   //   if (found) {
+//   //     return [item, found];
+//   //   }
+//   //   comp.push(sum-item);
+//   // }
 
-  // my solution, for of + find (quadratic)
-  // for (const [index, item] of arr.slice(0, arr.length-1).entries()) {
-  //   // console.log(`iterating item ${item}`);
-  //   const found = arr.slice(index + 1).find((arrItem) => arrItem === sum - item);
-  //   if (found) {
-  //     return [item, found];
-  //   }
-  // }
-  // return null;
+//   // my solution, for of + find (quadratic)
+//   // for (const [index, item] of arr.slice(0, arr.length-1).entries()) {
+//   //   // console.log(`iterating item ${item}`);
+//   //   const found = arr.slice(index + 1).find((arrItem) => arrItem === sum - item);
+//   //   if (found) {
+//   //     return [item, found];
+//   //   }
+//   // }
+//   // return null;
 
-  // for + set = O(n) + 0(1), linear
-  // const set = new Set([arr[0]]);
-  // for (const item of arr.slice(1)) {
-  //   if (set.has(item)) {
-  //     return true;
-  //   }
-  //   set.add(sum-item);
-  // }
+//   // for + set = O(n) + 0(1), linear
+//   // const set = new Set([arr[0]]);
+//   // for (const item of arr.slice(1)) {
+//   //   if (set.has(item)) {
+//   //     return true;
+//   //   }
+//   //   set.add(sum-item);
+//   // }
 
-  // const complements = new Set([sum-arr[0]]);
-  // return arr.slice(1).some((item) => {
-  //   console.log(`iterating item ${item}`);
-  //   if (complements.has(item)) {
-  //     return true;
-  //   }
-  //   complements.add(sum-item);
-  // })    
+//   // const complements = new Set([sum-arr[0]]);
+//   // return arr.slice(1).some((item) => {
+//   //   console.log(`iterating item ${item}`);
+//   //   if (complements.has(item)) {
+//   //     return true;
+//   //   }
+//   //   complements.add(sum-item);
+//   // })    
 
-  // const complements = new Set([sum-arr[0]]);
-  // return arr.slice(1).some((item) => {
-  //   console.log(`iterating item ${item}`);
-  //   return complements.has(item) || !complements.add(sum-item);
-  // })    
+//   // const complements = new Set([sum-arr[0]]);
+//   // return arr.slice(1).some((item) => {
+//   //   console.log(`iterating item ${item}`);
+//   //   return complements.has(item) || !complements.add(sum-item);
+//   // })    
 
-  // const complements = new Set([sum-arr[0]]);
-  // return arr.slice(1).some((item) => (
-  //   complements.has(item) || !complements.add(sum-item)
-  // ));
+//   // const complements = new Set([sum-arr[0]]);
+//   // return arr.slice(1).some((item) => (
+//   //   complements.has(item) || !complements.add(sum-item)
+//   // ));
 
-  const complements = new Set([sum-arr[0]]);
-  const found = arr.slice(1).find((item) => {
-    // console.log(`iterating item ${item}`);
-    if (complements.has(item)) {
-      return true;
-    }
-    complements.add(sum-item);
-  });
-  return found ? [found, sum-found] : null; 
-}
+//   // const complements = new Set([sum-arr[0]]);
+//   // const found = arr.slice(1).find((item) => {
+//   //   // console.log(`iterating item ${item}`);
+//   //   if (complements.has(item)) {
+//   //     return true;
+//   //   }
+//   //   complements.add(sum-item);
+//   // });
+//   // return found ? [found, sum-found] : null; 
 
-// const sequence = [1, 2, 4, 2, 6];
-// const sum = 6;
 
-const sequence = Array(1e3).fill(0).map((_, index) => index);
-const penultimateIndex = sequence.length-2;
-const lastIndex = sequence.length-1;
-const sum = sequence[penultimateIndex] + sequence[lastIndex];
+// }
+
+/**
+ * For each element in the array, iterate every other element starting form
+ * index + 1 to find the match.
+ * This results in a quadratic time complexity (loop inside a loop).
+ *  */
+const findPair = (array: Array<number>, sum: number): Array<number> => {
+  const found = array.find((item, index) => (
+    array
+      .slice(index + 1)
+      .some((possibleMatch) => item + possibleMatch === sum)
+  ));
+  return found ? [found, sum-found] : []; 
+};
+
+console.log('\n'.repeat(3));
+
+// const sequence = Array(1e3).fill(0).map((_, index) => index);
+// const penultimateIndex = sequence.length-2;
+// const lastIndex = sequence.length-1;
+// const sum = sequence[penultimateIndex] + sequence[lastIndex];
+
+const sequence = [1, 2, 3];
+const sum = 5;
 
 console.time('id');
-const yes = findPair(sequence, sum);
+const result = findPair(sequence, sum);
 console.timeEnd('id');
 
-console.log(yes);
+console.log(result);
 
-console.log('\n\n\n Finished');
+console.log('\n'.repeat(3));
