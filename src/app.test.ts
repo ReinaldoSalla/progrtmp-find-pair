dee52a4059508bb76ce9926d66fcd571a4208a28
Reinaldo Salla <reinaldosalla95@gmail.com>
2021-02-19 08:31:20 -0300

    starting to add unit testing

---
+++ src/app.test.ts
@@ -0,0 +1,12 @@
+import findPair from './app';
+
+describe('findPair', () => {
+  it('Should be true', () => {
+    //arrange
+
+    // act
+
+    //assert
+    
+  });
+});
--- src/app.ts
+++ src/app.ts
@@ -5,61 +5,60 @@ interface PairInfo {
   secondNumberIndex?: number;
 }
 
-// using for loop
-function findPair(numbers: Array<number>, sum: number) {
-  const lookup = new Set();
-  let secondNumber;
-  for (let i = 0; i < numbers.length; i++) {
-    if (lookup.has(sum - numbers[i])) {
-      secondNumber = numbers[i];
-    }
-    lookup.add(numbers[i])
-  }
-  if (!secondNumber) {
-    return null;
-  }
-  const firstNumber = sum - secondNumber;
-  const pairInfo: PairInfo = {};
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] === firstNumber && !('firstNumber' in pairInfo)) {
-      pairInfo.firstNumber = numbers[i];
-      pairInfo.firstNumberIndex = i;
-    } else if (numbers[i] === secondNumber && !('secondNumber' in pairInfo)) {
-      pairInfo.secondNumber = numbers[i];
-      pairInfo.secondNumberIndex = i;
-    }
-    if ('firstNumber' in pairInfo && 'secondNumber' in pairInfo) {
-      return pairInfo;
-    }
-  }
-}
-
-// using array iteration
 // function findPair(numbers: Array<number>, sum: number) {
 //   const lookup = new Set();
-//   const found = numbers.find((item) => {
-//     if (lookup.has(sum - item)) {
-//       return true;
+//   let secondNumber;
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (lookup.has(sum - numbers[i])) {
+//       secondNumber = numbers[i];
+//       break;
 //     }
-//     lookup.add(item)
-//   });
-//   if (!found) {
+//     lookup.add(numbers[i])
+//   }
+//   if (!secondNumber) {
 //     return null;
 //   }
-//   const secondNumber = found;
-//   const firstNumber = sum - found;
-//   const pairInfo = numbers.reduce((acc: PairInfo, curr, index) => {
-//     if (curr === firstNumber && !('firstNumber' in acc)) {
-//       acc.firstNumber = curr;
-//       acc.firstNumberIndex = index;
-//     } else if (curr === secondNumber && !('secondNumber' in acc)) {
-//       acc.secondNumber = curr;
-//       acc.secondNumberIndex = index;
+//   const firstNumber = sum - secondNumber;
+//   const pairInfo: PairInfo = {};
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] === firstNumber && !('firstNumber' in pairInfo)) {
+//       pairInfo.firstNumber = numbers[i];
+//       pairInfo.firstNumberIndex = i;
+//     } else if (numbers[i] === secondNumber && !('secondNumber' in pairInfo)) {
+//       pairInfo.secondNumber = numbers[i];
+//       pairInfo.secondNumberIndex = i;
 //     }
-//     return acc;
-//   }, {});
-//   return pairInfo;
+//     if ('firstNumber' in pairInfo && 'secondNumber' in pairInfo) {
+//       return pairInfo;
+//     }
+//   }
 // }
+
+function findPair(numbers: Array<number>, sum: number) {
+  const lookup = new Set();
+  const found = numbers.find((item) => {
+    if (lookup.has(sum - item)) {
+      return true;
+    }
+    lookup.add(item)
+  });
+  if (!found) {
+    return null;
+  }
+  const secondNumber = found;
+  const firstNumber = sum - found;
+  const pairInfo = numbers.reduce((acc: PairInfo, curr, index) => {
+    if (curr === firstNumber && !('firstNumber' in acc)) {
+      acc.firstNumber = curr;
+      acc.firstNumberIndex = index;
+    } else if (curr === secondNumber && !('secondNumber' in acc)) {
+      acc.secondNumber = curr;
+      acc.secondNumberIndex = index;
+    }
+    return acc;
+  }, {});
+  return pairInfo;
+}
 
 // const numbers = [0, 1, 2, 3];
 // const sum = 5;
@@ -73,13 +72,15 @@ const sum = 2;
 // const numbers = [1, 1];
 // const sum = 100;
 
-// const numbers = Array(1e3).fill(0).map((_, index) => index);
+// const numbers = Array(10e6).fill(0).map((_, index) => index);
 // const penultimateIndex = numbers.length-2;
 // const lastIndex = numbers.length-1;
 // const sum = numbers[penultimateIndex] + numbers[lastIndex];
 
-console.time('id');
+// console.time('findPair');
 const result = findPair(numbers, sum);
-console.timeEnd('id');
+// console.timeEnd('findPair');
 
 console.log(result);
+
+export default findPair;
